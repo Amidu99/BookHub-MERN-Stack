2b9cc2bbc66c694697a12bdd61fbd10b7a0a21e1
Amidu99 <amidushamika@gmail.com>
2024-08-05 16:47:33 +0530

    add get book by id

--- Backend/index.js
+++ Backend/index.js
@@ -53,6 +53,18 @@ app.get('/books', async (request, response) => {
   }
 });
 
+// Route for Get One Book from db by id
+app.get('/books/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
