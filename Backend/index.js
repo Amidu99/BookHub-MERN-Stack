977f37fe88d2a7c3836b05cf1d182f87237b0756
Amidu99 <amidushamika@gmail.com>
2024-08-05 17:21:28 +0530

    add middleware for hande CORS policy

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use('/books', booksRoute);
+
+// Middleware for handling CORS POLICY
+// Allow All Origins with Default of cors(*)
+app.use(cors());
 
 app.get('/', (request, response) => {
   console.log(request);
