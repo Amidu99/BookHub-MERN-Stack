4fd537e70a981a5df880a5523760bedd3d83bb81
Amidu99 <amidushamika@gmail.com>
2024-08-05 20:47:27 +0530

    add pages

---
+++ Frontend/src/pages/DeleteBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteBook = () => {
+  return (
+    <div>
+      DeleteBook
+    </div>
+  )
+}
+
+export default DeleteBook
---
+++ Frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowBook = () => {
+  return (
+    <div>
+      ShowBook
+    </div>
+  )
+}
+
+export default ShowBook
