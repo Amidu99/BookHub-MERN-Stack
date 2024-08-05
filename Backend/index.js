8949495d3ed3838d826af5de830035c7b1affdf3
Amidu99 <amidushamika@gmail.com>
2024-08-05 15:15:04 +0530

    add first http route

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express from 'express';
+import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('BookHub MERN Stack Application');
+});
+
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+  console.log(`App is listening to port: ${PORT}`);
 });
