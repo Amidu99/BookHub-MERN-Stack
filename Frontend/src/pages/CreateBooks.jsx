0700ea81cd10dca811d197c54ee8c3548b949373
Amidu99 <amidushamika@gmail.com>
2024-08-06 16:44:09 +0530

    update CreateBooks

--- Frontend/src/pages/CreateBooks.jsx
+++ Frontend/src/pages/CreateBooks.jsx
@@ -29,7 +29,6 @@ const CreateBooks = () => {
       })
       .catch((error) => {
         setLoading(false);
-        //alert('An error happened. Please Chack console');
         enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
@@ -37,17 +36,19 @@ const CreateBooks = () => {
 
   return (
     <div className='p-4'>
-      <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
+      <div className="relative flex items-center mb-10">
+        <BackButton />
+        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-4xl font-mono font-bold text-sky-500">Add New Book</h1>
+      </div>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+      <div className='flex flex-col border-4 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-xl'
           />
         </div>
         <div className='my-4'>
@@ -56,7 +57,7 @@ const CreateBooks = () => {
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-gray-500 px-4 py-2  w-full rounded-xl'
           />
         </div>
         <div className='my-4'>
@@ -65,10 +66,10 @@ const CreateBooks = () => {
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-gray-500 px-4 py-2  w-full rounded-xl'
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+        <button className='bg-sky-400 text-white p-2 m-8 rounded-xl hover:bg-green-500 hover:shadow-lg transition duration-300 ease-in-out' onClick={handleSaveBook}>
           Save
         </button>
       </div>
