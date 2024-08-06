6fd158a50f3013502718c7ca3e9365addaa26089
Amidu99 <amidushamika@gmail.com>
2024-08-06 16:19:01 +0530

    update DeleteBook

--- Frontend/src/pages/DeleteBook.jsx
+++ Frontend/src/pages/DeleteBook.jsx
@@ -22,21 +22,21 @@ const DeleteBook = () => {
       })
       .catch((error) => {
         setLoading(false);
-        //alert('An error happened. Please Check console');
         enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
   return (
     <div className='p-4'>
-      <BackButton />
-      <h1 className='text-3xl my-4'>Delete Book</h1>
+      <div className="relative flex items-center mb-10">
+        <BackButton />
+        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-4xl font-mono font-bold text-sky-500">Delete Book</h1>
+      </div>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-        <h3 className='text-2xl'>Are You Sure You want to delete this book?</h3>
-
+      <div className='flex flex-col items-center border-4 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl text-red-500 font-sans font-semibold'>Are You Sure You want to delete this book?</h3>
         <button
-          className='p-4 bg-red-600 text-white m-8 w-full'
+          className='p-3 bg-red-600 text-white m-8 w-9/12 rounded-xl'
           onClick={handleDeleteBook}
         >
           Yes, Delete it
