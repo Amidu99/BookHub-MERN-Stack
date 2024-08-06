7616b174e5f373bae6c8b0844c2b75ba64343d4b
Amidu99 <amidushamika@gmail.com>
2024-08-06 15:52:51 +0530

    update ShowBook

--- Frontend/src/pages/ShowBook.jsx
+++ Frontend/src/pages/ShowBook.jsx
@@ -25,35 +25,39 @@ const ShowBook = () => {
 
   return (
     <div className='p-4'>
-      <BackButton />
-      <h1 className='text-3xl my-4'>Show Book</h1>
+      <div className="relative flex items-center mb-10">
+        <BackButton />
+        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-4xl font-mono font-bold text-sky-500">Books Info</h1>
+      </div>
       {loading ? (
         <Spinner />
       ) : (
-        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Id</span>
-            <span>{book._id}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Title</span>
-            <span>{book.title}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Author</span>
-            <span>{book.author}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
-            <span>{book.publishYear}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Create Time</span>
-            <span>{new Date(book.createdAt).toString()}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
-            <span>{new Date(book.updatedAt).toString()}</span>
+        <div className="flex justify-center">
+          <div className='flex flex-col border-4 border-sky-400 rounded-xl w-fit p-4'>
+            <div className='my-3'>
+              <span className='text-xl mr-4 text-sky-500 font-sans font-semibold'>ID</span>
+              <span className='text-base font-medium text-cyan-700'>{book._id}</span>
+            </div>
+            <div className='my-3'>
+              <span className='text-xl mr-4 text-sky-500 font-sans font-semibold'>Title</span>
+              <span className='text-base font-medium text-cyan-700'>{book.title}</span>
+            </div>
+            <div className='my-3'>
+              <span className='text-xl mr-4 text-sky-500 font-sans font-semibold'>Author</span>
+              <span className='text-base font-medium text-cyan-700'>{book.author}</span>
+            </div>
+            <div className='my-3'>
+              <span className='text-xl mr-4 text-sky-500 font-sans font-semibold'>Publish Year</span>
+              <span className='text-base font-medium text-cyan-700'>{book.publishYear}</span>
+            </div>
+            <div className='my-3'>
+              <span className='text-xl mr-4 text-sky-500 font-sans font-semibold'>Create Time</span>
+              <span className='text-base font-medium text-cyan-700'>{new Date(book.createdAt).toString()}</span>
+            </div>
+            <div className='my-3'>
+              <span className='text-xl mr-4 text-sky-500 font-sans font-semibold'>Last Update Time</span>
+              <span className='text-base font-medium text-cyan-700'>{new Date(book.updatedAt).toString()}</span>
+            </div>
           </div>
         </div>
       )}
