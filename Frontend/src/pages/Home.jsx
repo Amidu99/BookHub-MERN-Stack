72178cf346023ea40e198b27367f54f4281ed612
Amidu99 <amidushamika@gmail.com>
2024-08-06 15:01:41 +0530

    add navbar

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Spinner from '../components/Spinner';
+import { Disclosure } from '@headlessui/react';
 import { Link } from 'react-router-dom';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { BsTable, BsGridFill, BsPlusSquare } from "react-icons/bs";
+import Spinner from '../components/Spinner';
 import BooksTable from '../components/home/BooksTable';
 import BooksCard from '../components/home/BooksCard';
 
@@ -26,34 +27,59 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-center items-center gap-x-4'>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('table')}
-        >
-          Table
-        </button>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('card')}
-        >
-          Card
-        </button>
+    <div>
+      <Disclosure as="nav" className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center sm:justify-start content-center">
+              <div className="flex flex-shrink-0 items-center">
+                <img
+                  alt="BookHub"
+                  src="./src/assets/icon.svg"
+                  className="h-8 w-auto"
+                />
+              </div>
+              <div className="text-gray-400 hover:text-white pl-4 text-2xl font-bold">BookHub</div>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex space-x-4 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <button
+                type="button"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                onClick={() => setShowType('table')}
+              >
+                <BsTable aria-hidden="true" className="h-6 w-6" />
+              </button>
+
+              <button
+                type="button"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                onClick={() => setShowType('card')}
+              >
+                <BsGridFill aria-hidden="true" className="h-6 w-6" />
+              </button>
+              <Link  to='/books/create'>
+              <button
+                type="button"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                onClick={() => Link}
+              >
+                <BsPlusSquare aria-hidden="true" className="h-6 w-6" />
+              </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+      <div className='p-4 bg-sky-200'>
+        <h1 className='text-4xl my-8 font-mono font-bold text-center'>Books List</h1>
+        {loading ? (
+          <Spinner />
+        ) : showType === 'table' ? (
+          <BooksTable books={books} />
+        ) : (
+          <BooksCard books={books} />
+        )}
       </div>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
-        <Link to='/books/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl' />
-        </Link>
-      </div>
-      {loading ? (
-        <Spinner />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
-      ) : (
-        <BooksCard books={books} />
-      )}
     </div>
   );
 };
