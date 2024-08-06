f0b07aa1ca9a11ed09b4bd0cbeec3310c3e1916e
Amidu99 <amidushamika@gmail.com>
2024-08-06 16:10:05 +0530

    update EditBook

--- Frontend/src/pages/EditBook.jsx
+++ Frontend/src/pages/EditBook.jsx
@@ -52,17 +52,19 @@ const EditBook = () => {
 
   return (
     <div className='p-4'>
-      <BackButton />
-      <h1 className='text-3xl my-4'>Edit Book</h1>
+      <div className="relative flex items-center mb-10">
+        <BackButton />
+        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-4xl font-mono font-bold text-sky-500">Edit Book Info</h1>
+      </div>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+      <div className='flex flex-col border-4 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-xl'
           />
         </div>
         <div className='my-4'>
@@ -71,7 +73,7 @@ const EditBook = () => {
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-gray-500 px-4 py-2  w-full rounded-xl'
           />
         </div>
         <div className='my-4'>
@@ -80,10 +82,10 @@ const EditBook = () => {
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className='border-2 border-gray-500 px-4 py-2  w-full rounded-xl'
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+        <button className='bg-sky-400 text-white p-2 m-8 rounded-xl hover:bg-sky-500 hover:shadow-lg transition duration-300 ease-in-out' onClick={handleEditBook}>
           Save
         </button>
       </div>
